f7cea84f84897dfc2553d8654f0452b27bcca7de
GD <GD@DESKTOP-BKGIJ3T>
2025-02-19 11:26:27 +0900

    '20250219'

---
+++ chap12/src/ex1_List/ListEx1.java
@@ -0,0 +1,37 @@
+package ex1_List;
+
+import java.util.ArrayList;
+
+/*
+ * Collection 인터페이스 : 객체를 여러개저장할 수 있는 객체]
+ * 
+ * 
+ * List : Collection 인터페이스의 하위 
+ * 			객체들 저장할 때 저장된 순서를 유지 -> 인덱스 사용 가능
+ * 
+ * 		구현클래스 : ArrayList , Vector , LinkedList..
+ * 			Vector : Collection프레임워크 이전의 클래스/
+ * 					Collection프레임워크에서 제공되는 함수ㅘ 기존에사용했던함수 혼합			
+ * 
+ * 
+ * Set : Collection 인터페이스의 하위 
+ * 			객체 중복저장 불가능
+ * 		구현클래스 : HashSet , TreeSet , LinkedHashSet
+ * 
+ * 
+ * 
+ * 
+ */
+public class ListEx1 {
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(1);list.add(2);;list.add(5);
+		list.add(4);list.add(3);;list.add(0);
+		list.add(3);
+		System.out.println(list);
+
+
+	}
+
+}
