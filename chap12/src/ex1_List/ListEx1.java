c8a6ca46c7250bccbd89bafda2d200b9e60d6e48
GD <GD@DESKTOP-BKGIJ3T>
2025-02-19 11:52:14 +0900

    '244'

--- chap12/src/ex1_List/ListEx1.java
+++ chap12/src/ex1_List/ListEx1.java
@@ -1,6 +1,9 @@
 package ex1_List;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 /*
  * Collection 인터페이스 : 객체를 여러개저장할 수 있는 객체]
@@ -32,6 +35,49 @@ public class ListEx1 {
 		list.add(1);list.add(2);;list.add(5);
 		list.add(4);list.add(3);;list.add(0);
 		list.add(3);
+//		list.add("4");
 		System.out.println(list);
+		
+		//인덱스로 값 찾기
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println
+			(i+" : "+list.get(i));
+		}
+		
+		//정렬하기
+		//Collection : 인터페이스
+		//Collections : 클래스( Collection 객체의  기능 보조)
+		Collections.sort(list);
+		System.out.println("sort "+list);
+		
+		Collections.sort(list,Collections.reverseOrder());//내림차순
+		System.out.println("reverseOrder : "+list);
+		
+		Collections.shuffle(list);//섞기
+		System.out.println("Shuffle : "+list);
+		
+		//인덱스 범위지정해 객체조회
+		System.out.println("1번 인덱스부터 3번 인덱스의 객체조회 : "+
+							list.subList(1,4));
+		
+		//1~3번인덱스로 리스트생성 ( but 둘은 같은곳을 참조하지만)
+		//list2는 1~3번인덱스만을 참조함
+		//list2에 추가하면 list에도 영향이있는것!!
+		List<Integer> list2 = list.subList(1, 4);
+		System.out.println(list2);
+		list2.add(100);
+		System.out.println("list :"+list);
+		System.out.println("list2 : "+list2);
+		
+		System.out.println("==새로운 객체를 만들어서 반환==");
+		list2 = new ArrayList<>(list.subList(1, 4));
+		list2.add(222);
+		System.out.println("list : "+list);
+		System.out.println("list2 : "+list2);
+		
+		
+		
+		
+		
 	}
 }
